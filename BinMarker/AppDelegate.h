aa9163a42c8d7bf8f351e64062f7bdfd765064bf
彭子上 <pengzishang@pzs.local>
2016-11-17 16:11:06 +0800

    Initial Commit

---
+++ BinMarker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BinMarker
+//
+//  Created by 彭子上 on 2016/11/17.
+//  Copyright © 2016年 彭子上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BinMarker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BinMarker
+//
+//  Created by 彭子上 on 2016/11/17.
+//  Copyright © 2016年 彭子上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
